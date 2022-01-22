66c204f791d1747e2d72600d79918821f0ea02d7
youben11 <ayouben9@gmail.com>
2022-01-23 00:17:16 +0100

    chore: fix formatting

--- src/core/loader/index.js
+++ src/core/loader/index.js
@@ -1,23 +1,27 @@
-const fs = require("fs");
-const ascii = require("ascii-table");
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v9");
+const fs = require('fs');
+const Ascii = require('ascii-table');
+const {REST} = require('@discordjs/rest');
+const {Routes} = require('discord-api-types/v9');
 
 /**
  * Load slash commands from files and register them
+ * @param {*} client
+ * @param {*} token
+ * @param {*} clientId
+ * @param {*} guildId
  */
 function loadCommands(client, token, clientId, guildId) {
-  let commands = [];
+  const commands = [];
   // Prepare ascii table for logging
-  const table = new ascii().setHeading("Slash Commands", "Load Status");
+  const table = new Ascii().setHeading('Slash Commands', 'Load Status');
   // read folder of commands
-  const categoryFolders = fs.readdirSync("src/commands");
+  const categoryFolders = fs.readdirSync('src/commands');
   // iterate over the categories of commands
   for (const folder of categoryFolders) {
     // get the commands of the category
     const commandFiles = fs
-      .readdirSync(`src/commands/${folder}`)
-      .filter((file) => file.endsWith(".js"));
+        .readdirSync(`src/commands/${folder}`)
+        .filter((file) => file.endsWith('.js'));
     // iterate over the commands
     for (const file of commandFiles) {
       const command = require(`../../commands/${folder}/${file}`);
@@ -26,23 +30,23 @@ function loadCommands(client, token, clientId, guildId) {
         // add the command to the global list of commands
         client.commands.set(command.name, command);
         commands.push(command);
-        table.addRow(file, " Loaded ✔️");
+        table.addRow(file, ' Loaded ✔️');
       } else {
         table.addRow(
-          file,
-          "❌ => Missing a help.name or help.name is not in string"
+            file,
+            '❌ => Missing a help.name or help.name is not in string',
         );
       }
     }
   }
   console.log(table.toString());
   // create rest client
-  const rest = new REST({ version: "9" }).setToken(token);
+  const rest = new REST({version: '9'}).setToken(token);
 
   // register commands
   rest
-    .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-    .then(() => console.log("Successfully registered application commands."))
-    .catch(console.error);
+      .put(Routes.applicationGuildCommands(clientId, guildId), {body: commands})
+      .then(() => console.log('Successfully registered application commands.'))
+      .catch(console.error);
 }
-module.exports = { loadCommands };
+module.exports = {loadCommands};
