c67a987701021c7049f2d56a6f0986ddee17a2e3
Azzedde <ja_aitsaid@esi.dz>
2022-01-26 20:59:16 +0100

    Push the Listeners in bot.js

--- src/bot.js
+++ src/bot.js
@@ -4,8 +4,12 @@ const {clientId, guildId, token} = require('../config.json');
 const {loadCommands} = require('./core/loader/index');
 
 // Create a new client instance
-const client = new Client({
-  intents: [Intents.FLAGS.GUILDS],
+const client = new Client({intents: [
+  Intents.FLAGS.GUILDS,
+  Intents.FLAGS.GUILD_MESSAGES,
+  Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  Intents.FLAGS.GUILD_VOICE_STATES],
+partials: ['USER', 'REACTION', 'MESSAGE'],
 });
 client.commands = new Collection();
 
@@ -33,6 +37,50 @@ client.on('interactionCreate', async (interaction) => {
       content: 'There was an error while executing this command!',
       ephemeral: true,
     });
+  }
+});
+/* Listener for presence in Voice Channels */
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+  const newUserChannel = newMember.channelId;
+
+  if (newUserChannel === '933099887799705624') {
+    try {
+      if (fs.existsSync('pv.json')) {
+        fs.readFile('pv.json', 'utf-8', (err, data) => {
+          if (err) {
+            throw err;
+          }
+
+          // parse JSON object
+          const pv = JSON.parse(data.toString());
+          pv.Presents.push(newMember.member.user.tag);
+          console.log(pv);
+          fs.writeFileSync('pv.json', JSON.stringify(pv));
+        });
+      }
+    } catch (err) {
+      console.log('Meeting not already initialized!');
+    }
+  }
+});
+
+/* Listener for Important Message Reaction */
+
+client.on('message', (message) => {
+  if (fs.existsSync('pv.json')) {
+    const data = fs.readFileSync('pv.json', 'utf-8');
+    const pv = JSON.parse(data.toString());
+    if (message.channelId === pv.Thread) {
+      client.on('messageReactionAdd', (reaction, user) => {
+        if (reaction.message.channelId === pv.Thread) {
+          if (reaction.emoji.name === '✍️') {
+            pv.Points.push(reaction.message.content);
+            fs.writeFileSync('pv.json', JSON.stringify(pv));
+            console.log(pv);
+          }
+        }
+      });
+    }
   }
 });
 
