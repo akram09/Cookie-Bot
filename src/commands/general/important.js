2bbac4e45cae55c3d1f8f661a1acebac364258a6
akram09 <a.boutouchent@esi-sba.dz>
2022-01-28 02:09:52 +0100

    fix: Fixing the eslint problem

--- src/commands/general/important.js
+++ src/commands/general/important.js
@@ -1,52 +1,47 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
+const {SlashCommandBuilder} = require('@discordjs/builders');
+const {MessageEmbed} = require('discord.js');
 
 const fs = require('fs');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('important')
-		.setDescription('Saves your important message')
-		.addStringOption(option =>
-			option.setName('message')
-				.setDescription('The Message you want to save')
-				.setRequired(true)),
-	async execute(interaction) {
-        const m = interaction.options.getString('message')
-		
-		if(fs.existsSync('pv.json')){
-			const data = fs.readFileSync('pv.json', 'utf-8')
+  data: new SlashCommandBuilder()
+      .setName('important')
+      .setDescription('Saves your important message')
+      .addStringOption((option) =>
+        option.setName('message')
+            .setDescription('The Message you want to save')
+            .setRequired(true)),
+  async execute(interaction) {
+    const m = interaction.options.getString('message');
 
-		
-			// parse JSON object
-			const pv = JSON.parse(data.toString());
-			if (interaction.channelId === pv.Thread){
-			pv.Points.push(m)
-			console.log(pv);
-            fs.writeFileSync('pv.json', JSON.stringify(pv))
-			const embed = new MessageEmbed()
-			.setColor('#0099ff')
-			.setTitle('Important Message Saving')
-			.setDescription('The following message will be saved in the PV of the Meeting:')
-			.setThumbnail('https://www.freeiconspng.com/uploads/megaphone-message-news-promotion-speaker-icon--17.png')
-			.addFields(
-				{name: 'Message', value: m },
-	
-			)
-	
-			interaction.reply({ embeds:[embed] });
-			}
-			else{
-				interaction.reply('It seems that you used this command outside a Meeting Thread !');
-			}
-		}
-		else{
-
-		
-		await interaction.reply('You\'re not inside a Meeting Thread!')
-	}	
-		
+    if (fs.existsSync('pv.json')) {
+      const data = fs.readFileSync('pv.json', 'utf-8');
 
 
-	},
+      // parse JSON object
+      const pv = JSON.parse(data.toString());
+      if (interaction.channelId === pv.Thread) {
+        pv.Points.push(m);
+        console.log(pv);
+        fs.writeFileSync('pv.json', JSON.stringify(pv));
+        const embed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle('Important Message Saving')
+            .setDescription('The following message will be'+
+            ' saved in the PV of the Meeting:')
+            .setThumbnail('https://www.freeiconspng.com/uploads/megaphone-message-news-promotion-speaker-icon--17.png')
+            .addFields(
+                {name: 'Message', value: m},
+
+            );
+
+        interaction.reply({embeds: [embed]});
+      } else {
+        interaction.reply('It seems that you used this'+
+         ' command outside a Meeting Thread !');
+      }
+    } else {
+      await interaction.reply('You\'re not inside a Meeting Thread!');
+    }
+  },
 };
--- src/commands/general/next.js
+++ src/commands/general/next.js
@@ -1,49 +1,48 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed } = require('discord.js');
+const {SlashCommandBuilder} = require('@discordjs/builders');
+const {MessageEmbed} = require('discord.js');
 
 const fs = require('fs');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('next')
-		.setDescription('Saves you message as a next point to discuss in future meetings')
-		.addStringOption(option =>
-			option.setName('message')
-				.setDescription('The Message you want to save for future meetings')
-				.setRequired(true)),
-	async execute(interaction) {
-        const m = interaction.options.getString('message')
-			if(fs.existsSync('pv.json')){
-			const data = fs.readFileSync('pv.json', 'utf-8')
+  data: new SlashCommandBuilder()
+      .setName('next')
+      .setDescription('Saves you message as a next'+
+      ' point to discuss in future meetings')
+      .addStringOption((option) =>
+        option.setName('message')
+            .setDescription('The Message you want to save for future meetings')
+            .setRequired(true)),
+  async execute(interaction) {
+    const m = interaction.options.getString('message');
+    if (fs.existsSync('pv.json')) {
+      const data = fs.readFileSync('pv.json', 'utf-8');
 
-		
-			// parse JSON object
-			const pv = JSON.parse(data.toString());
-            if(interaction.channelId === pv.Thread){
-			pv.Next.push(m);
-			console.log(pv);
-            fs.writeFileSync('pv.json', JSON.stringify(pv))
-            const embed = new MessageEmbed()
+
+      // parse JSON object
+      const pv = JSON.parse(data.toString());
+      if (interaction.channelId === pv.Thread) {
+        pv.Next.push(m);
+        console.log(pv);
+        fs.writeFileSync('pv.json', JSON.stringify(pv));
+        const embed = new MessageEmbed()
             .setColor('#0099ff')
             .setTitle('Next Message Saving')
-            .setDescription('The following message will be saved in the PV to discuss in future Meetings:')
+            .setDescription('The following message will be saved'+
+             ' in the PV to discuss in future Meetings:')
             .setThumbnail('https://www.freeiconspng.com/uploads/megaphone-message-news-promotion-speaker-icon--17.png')
             .addFields(
-                {name: 'Message', value: m },
-    
-            )
-    
-            interaction.reply({ embeds:[embed] });
-            }
-            else{
-                interaction.reply('It seems that you used this command outside a Meeting Thread !')
-            }
+                {name: 'Message', value: m},
 
-		}	
-		else {
-            interaction.reply('It seems that you used this command outside a Meeting Thread !')
-        }	
+            );
 
-
-	},
+        interaction.reply({embeds: [embed]});
+      } else {
+        interaction.reply('It seems that you used'+
+        ' this command outside a Meeting Thread !');
+      }
+    } else {
+      interaction.reply('It seems that you used'+
+        ' this command outside a Meeting Thread !');
+    }
+  },
 };
